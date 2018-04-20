a7df73f15ac928b631d817bf99023e5c4823eed7
gulico <767293861@qq.com>
2018-04-20 09:06:19 +0800

    第五次试验

---
+++ Task_5/ex_1/ex_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ex_1
+{
+    class Program
+    {
+        class Person {
+            public string Name = "未命名";
+            public int age = 0;
+            public string sex = "未知";
+        }
+        class Student : Person{
+            public double[] Scores = new double[5]{0,0,0,0,0};
+            public Student() { }
+            public Student(string newName, int newage, string newSex)
+            {
+                this.Name = newName;
+                this.age = newage;
+                this.sex = newSex;
+            }
+            public Student(string newName, int newage, string newSex, double[] newScores)
+            {
+                this.Name = newName;
+                this.age = newage;
+                this.sex = newSex;
+                this.SetScores(newScores);
+            }
+            public void SetScores(double [] newScores){
+                this.Scores = newScores;
+            }
+            public double GetAverage() {
+                return this.Scores.Average();
+            }
+            public void PrintAverage() { 
+                Console.WriteLine(this.Name+"的平均成绩为："+this.GetAverage());            }
+        }
+        static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student("张三",19,"男");
+            double[] scores2 = new double[5] { 30, 96, 40, 50, 60 };
+            s2.SetScores(scores2);
+            double [] scores3 = new double[5]{90,98,92,93,60};
+            Student s3 = new Student("李四",20,"女",scores3);
+            s1.PrintAverage();
+            s2.PrintAverage();
+            s3.PrintAverage();
+            Console.Read();
+        }
+    }
+}
---
+++ Task_5/ex_2/ex_2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ex_2
+{
+    class Program
+    {
+        class Digit {
+            private char Number;
+            public void Input(){
+                Console.Write("请输入一个0-9的数字：");
+                this.Number = Convert.ToChar(Console.ReadLine().Trim());
+            }
+            public void Output() {
+                Console.WriteLine("Number的值为：" + Number);
+            }
+            public char GetNumber() {
+                return Number;
+            }
+        }
+        class MultiDigit : Digit{ 
+            private char[] MultiNumber = new char[2];
+            public void Input() {
+                Console.Write("请输入一个0-99的数字：");
+                string str = Console.ReadLine().Trim();
+                this.MultiNumber = str.ToCharArray(0, str.Length);
+                    
+            }
+            public void Input(Digit d1,Digit d2)
+            {
+                MultiNumber[0] = d1.GetNumber();
+                MultiNumber[1] = d2.GetNumber();
+            }
+            public void Output()
+            {
+                Console.WriteLine("Number的值为：" + this.MultiNumber[0]+this.MultiNumber[1]);
+            }
+            public char[] Output(int flag)
+            {
+                  return MultiNumber;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Digit d1 = new Digit();
+            d1.Input();
+            Console.WriteLine("d1：");
+            d1.Output();
+            Digit d2 = new Digit();
+            d2.Input();
+            Console.WriteLine("d2：");
+            d2.Output();
+            MultiDigit d3 = new MultiDigit();
+            d3.Input();
+            Console.WriteLine("d3：");
+            d3.Output();
+
+            MultiDigit d4 = new MultiDigit();
+            d4.Input(d1,d2);
+            Console.WriteLine("d4：");
+            d4.Output();
+            Console.WriteLine("第二种output：" + d4.Output(1)[0]+d4.Output(0)[1]);
+            Console.ReadLine();
+        }
+    }
+}
